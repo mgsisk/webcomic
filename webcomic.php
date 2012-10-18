a7b566d99791626c93e22ab9002f2abb3be76fe0
Michael Sisk <contact@mgsisk.com>
2012-10-17 23:38:25 -0400

    Update version number for release.

--- webcomic.php
+++ webcomic.php
@@ -4,7 +4,7 @@ Text Domain: webcomic
 Plugin Name: Webcomic
 Plugin URI: http://webcomic.nu
 Description: Comic publishing power for the web.
-Version: 4
+Version: 4.0.2
 Author: Michael Sisk
 Author URI: http://mgsisk.com
 License: GPL2
@@ -47,7 +47,7 @@ class Webcomic {
 	/** Internal version number.
 	 * @var string
 	 */
-	protected static $version = '4';
+	protected static $version = '4.0.2';
 	
 	/** Absolute path to the Webcomic directory.
 	 * @var string
