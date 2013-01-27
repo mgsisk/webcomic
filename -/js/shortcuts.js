318c9684a42ffc7c4336b3f8e72341f542fc553d
Michael Sisk <contact@mgsisk.com>
2013-01-27 10:14:37 -0500

    Correct keyboard shortcut functionality.
    Ensures keyboard shortcuts work for both standard and dynamic
    navigation. Also adds support for specifying keyboard navigation blocks
    with the data-webcomic-shortcuts attribute.

--- -/js/shortcuts.js
+++ -/js/shortcuts.js
@@ -1,9 +1,27 @@
 /** Handle keyboard shortcut navigation. */
 jQuery( function( $ ) {
-	$.hotkeys.add( 'right', function() { if ( $( '.next-webcomic-link' ).attr( 'href' ) ) { $( '.next-webcomic-link' ).first().trigger( 'click' ); } } );
-	$.hotkeys.add( 'left', function() { if ( $( '.previous-webcomic-link' ).attr( 'href' ) ) { $( '.previous-webcomic-link' ).first().trigger( 'click' ); } } );
-	$.hotkeys.add( 'shift+right', function() { if ( $( '.last-webcomic-link' ).attr( 'href' ) ) { $( '.last-webcomic-link' ).first().trigger( 'click' ); } } );
-	$.hotkeys.add( 'shift+left', function() { if ( $( '.first-webcomic-link' ).attr( 'href' ) ) { $( '.first-webcomic-link' ).first().trigger( 'click' ); } } );
-	$.hotkeys.add( 'shift+up', function() { if ( $( '.purchase-webcomic-link' ).attr( 'href' ) ) { $( '.purchase-webcomic-link' ).first().trigger( 'click' ); } } );
-	$.hotkeys.add( 'shift+down', function() { if ( $( '.random-webcomic-link' ).attr( 'href' ) ) { $( '.random-webcomic-link' ).first().trigger( 'click' ); } } );
+	function webcomic_shortcut( target ) {
+		var $e;
+		
+		if ( $( '[data-webcomic-shortcuts] ' + target + '[href]' ).length ) {
+			$e = $( '[data-webcomic-shortcuts]:first ' + target + '[href]:first' );
+		} else {
+			$e = $( target + '[href]:first' );
+		}
+		
+		if ( $e.length ) {
+			if ( $e.closest( '[data-webcomic-container]' ).length ) {
+				$e.trigger( 'click' );
+			} else {
+				window.location.href = $e.attr( 'href' );
+			}
+		}
+	}
+	
+	$.hotkeys.add( 'right', function() { webcomic_shortcut( '.next-webcomic-link' ); } );
+	$.hotkeys.add( 'shift+right', function() { webcomic_shortcut( '.last-webcomic-link' ); } );
+	$.hotkeys.add( 'left', function() { webcomic_shortcut( '.previous-webcomic-link' ); } );
+	$.hotkeys.add( 'shift+left', function() { webcomic_shortcut( '.first-webcomic-link' ); } );
+	$.hotkeys.add( 'shift+down', function() { webcomic_shortcut( '.random-webcomic-link' ); } );
+	$.hotkeys.add( 'shift+up', function() { webcomic_shortcut( '.purchase-webcomic-link' ); } );
 } );
