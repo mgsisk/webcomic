e8cedff58d2be3257b370f5a1a91a18655b73dfe
Michael Sisk <contact@mgsisk.com>
2013-01-29 11:20:25 -0500

    Clean up.
    More OCD changes, nothing functional.

--- -/js/shortcuts.js
+++ -/js/shortcuts.js
@@ -19,9 +19,9 @@ jQuery( function( $ ) {
 	}
 	
 	$.hotkeys.add( 'right', function() { webcomic_shortcut( '.next-webcomic-link' ); } );
-	$.hotkeys.add( 'shift+right', function() { webcomic_shortcut( '.last-webcomic-link' ); } );
 	$.hotkeys.add( 'left', function() { webcomic_shortcut( '.previous-webcomic-link' ); } );
+	$.hotkeys.add( 'shift+right', function() { webcomic_shortcut( '.last-webcomic-link' ); } );
 	$.hotkeys.add( 'shift+left', function() { webcomic_shortcut( '.first-webcomic-link' ); } );
-	$.hotkeys.add( 'shift+down', function() { webcomic_shortcut( '.random-webcomic-link' ); } );
 	$.hotkeys.add( 'shift+up', function() { webcomic_shortcut( '.purchase-webcomic-link' ); } );
+	$.hotkeys.add( 'shift+down', function() { webcomic_shortcut( '.random-webcomic-link' ); } );
 } );
