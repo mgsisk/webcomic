4ab0c2df79fca8acd51568cf3a64025f5ceceaa4
Michael Sisk <me@mgsisk.com>
2017-12-05 12:09:24 -0500

    Add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 8,
+    ecmaFeatures: {
+      globalReturn: true,
+      impliedStrict: true
+    }
+  },
+  extends: 'wordpress'
+};
