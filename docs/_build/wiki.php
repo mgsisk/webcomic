b7787a32da67714210b2918f06dcb08e33e3ed1e
Michael Sisk <me@mgsisk.com>
2018-04-06 08:59:22 -0400

    Improve wiki builder
    Now we ignore the utility files generated by the API builder and build _Sidebar and _Footer files.

--- docs/_build/wiki.php
+++ docs/_build/wiki.php
@@ -24,9 +24,56 @@ foreach ( glob( $path . '/_wiki/*' ) as $file ) {
 $files = array_merge( glob( $path . '/_api/*.md' ), glob( $path . '/_pages/*.md' ) );
 
 foreach ( $files as $file ) {
+	if ( preg_match( '/^.+?\/!.[^\/]+\.md/', $file ) ) {
+		continue;
+	}
+
   // @codingStandardsIgnoreLine
 	$content = preg_replace( '/^---[\S\s]+---\s+/', '', file_get_contents( $file ) );
 
 	// @codingStandardsIgnoreLine
 	file_put_contents( str_replace( [ '_api', '_pages' ], '_wiki', $file ), $content );
 }
+
+$counter = 0;
+$sidebar = '';
+// @codingStandardsIgnoreLine
+$sections = json_decode( file_get_contents( $path . '/_data/sections.json' ) );
+
+foreach ( $sections as $label => $section ) {
+	if ( '_' === $label ) {
+		foreach ( $section as $subsection ) {
+			$counter++;
+			$sidebar .= "{$counter}. [{$subsection}](" . str_replace( ' ', '-', $subsection ) . ")\n";
+		}
+
+		continue;
+	}
+
+	$sidebar .= "\n### {$label}\n\n";
+
+	foreach ( $section as $subsection ) {
+		$sidebar .= "- [{$subsection}](" . str_replace( [ ' ', 'Character', 'Storyline' ], [ '-', 'Taxonomies', 'Taxonomies' ], $subsection ) . ")\n";
+	}
+}
+
+// @codingStandardsIgnoreLine
+file_put_contents( $path . '/_wiki/_Sidebar.md', $sidebar );
+
+$footer = '[![Support forum][img-1]][url-1]
+[![Discord server][img-2]][url-2]
+[![Contact Mike][img-3]][url-3]
+[![Support development][img-4]][url-4]
+
+[img-1]: https://img.shields.io/badge/support-forum-blue.svg
+[img-2]: https://img.shields.io/discord/361857773874446339.svg
+[img-3]: https://img.shields.io/badge/contact-mike-red.svg
+[img-4]: https://img.shields.io/badge/donate-paypal-yellow.svg
+[url-1]: https://wordpress.org/support/plugin/webcomic
+[url-2]: https://discord.gg/TNTfzzg
+[url-3]: mailto:help@mgsisk.com?subject=Webcomic%20Help
+[url-4]: https://mgsisk.com/#support
+';
+
+// @codingStandardsIgnoreLine
+file_put_contents( $path . '/_wiki/_Footer.md', $footer );
