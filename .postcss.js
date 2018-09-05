b1b233c64ba367dcc98e1d97a86e4f52ab224575
Michael Sisk <me@mgsisk.com>
2018-09-05 11:40:47 -0400

    Replace postcss-cssnext with postcss-preset-env
    postcss-cssnext has been deprecated

--- .postcss.js
+++ .postcss.js
@@ -6,7 +6,12 @@ module.exports = {
 		require( './.postcss.custom.js' ),
 		require( 'postcss-easings' ),
 		require( 'postcss-short' ),
-		require( 'postcss-cssnext' )(),
 		require( 'cssnano' )({autoprefixer: false})
+    require( 'postcss-preset-env' )({
+      stage: 0,
+      features: {
+        'custom-properties': {preserve: false},
+      },
+    }),
 	]
 };
