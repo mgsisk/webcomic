a7fb930f358546b396ab32a2e416d7838732041b
Michael Sisk <me@mgsisk.com>
2018-09-05 11:51:11 -0400

    Correct formatting errors

--- .postcss.js
+++ .postcss.js
@@ -6,12 +6,12 @@ module.exports = {
 		require( './.postcss.custom.js' ),
 		require( 'postcss-easings' ),
 		require( 'postcss-short' ),
-    require( 'postcss-preset-env' )({
-      stage: 0,
-      features: {
-        'custom-properties': {preserve: false},
-      },
-    }),
+		require( 'postcss-preset-env' )({
+			stage: 0,
+			features: {
+				'custom-properties': {preserve: false}
+			}
+		}),
 		require( 'cssnano' )({preset: 'default'})
 	]
 };
